1ce039c2c6cbc578f566425a52dadf7be4fb9978
kevinjk27 <52363107+kevinjk27@users.noreply.github.com>
2020-06-16 14:12:58 -0700

    updates isSorted inside ListNode & testcase

--- src/ListNode.java
+++ src/ListNode.java
@@ -1,48 +1,68 @@
+/*
+ * 06/11/2020
+ * Checks whether a linked list is sorted (non-decreasing) and returns true
+ * An empty list is considered to be sorted. 
+ */
+
 // ListNode is a class for storing a single node of a linked
 // list.  This node class is for a list of integer values.
 
 public class ListNode {
-    public int data;       // data stored in this node
-    public ListNode next;  // link to next node in the list
-    
-    // Reference to the first element:
-    private ListNode head;
+	public int data; // data stored in this node
+	public ListNode next; // link to next node in the list
 
-    // post: constructs a node with data 0 and null link
-    public ListNode() {
-        this(0, null);
-    }
+	// Reference to the first element:
+	private ListNode head;
 
-    // post: constructs a node with given data and null link
-    public ListNode(int data) {
-        this(data, null);
-    }
-
-    // post: constructs a node with given data and given link
-    public ListNode(int data, ListNode next) {
-        this.data = data;
-        this.next = next;
-    }
-
-
-	public void add(int number) {
-		ListNode newNode = new ListNode(number);
-		newNode.data=number;
-		newNode.next=null;
-		// if list is empty
-		if (head == null) {
-			head = newNode;
-		} else {
-			ListNode last = head;
-			while(last.next != null) {
-				last = last.next;
-			}
-			last.next = newNode;
-		}
+	// post: constructs a node with data 0 and null link
+	public ListNode() {
+		this(0, null);
 	}
-	private static ListNode front; 
 
-	public static boolean isSorted() {
+	// post: constructs a node with given data and null link
+	public ListNode(int data) {
+		this(data, null);
+	}
+
+	// post: constructs a node with given data and given link
+	public ListNode(int data, ListNode next) {
+		this.data = data;
+		this.next = next;
+	}
+
+	ListNode front; ; // first value in the list
+
+	// post: returns comma-separated, bracketed version of list
+    public String toString() {
+        if (front == null) {
+            return "[]";
+        } else {
+            String result = "[" + front.data;
+            ListNode current = front.next;
+            while (current != null) {
+                result += ", " + current.data;
+                current = current.next;
+            }
+            result += "]";
+            return result;
+        }
+    }
+
+    // post: appends the given value to the end of the list
+    public void add(int value) {
+        if (front == null) {
+            front = new ListNode(value);
+        } else {
+            ListNode current = front;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = new ListNode(value);
+        }
+    }
+	
+
+	public boolean isSorted() {
 		if (front == null) {
 			return true;
 		} 
@@ -58,20 +78,41 @@ public class ListNode {
 		return true;
 	}
 	
-	public String toString() {
-	    String result = data + " ";
-	    if (next != null) {
-	        result += next.toString();
-	    }
-	    return result;
-	}
 	
 	public static void main(String[] args) {
 		ListNode test1 = new ListNode();
-		test1.add(6);
-		test1.add(-3);
-		test1.add(5);
-		System.out.println("Good "+test1.toString());
+		test1.add(-632);
+		test1.add(-528);
+		test1.add(-348);
+		test1.add(-72);
+		test1.add(0);
+		test1.add(-8);
+		test1.add(9);
+		System.out.println(test1.toString() +"\nis sorted: " +test1.isSorted());
+		System.out.println();
+		
+		ListNode test2 = new ListNode();
+		test2.add(49);
+		test2.add(79);
+		test2.add(103);
+		test2.add(149);
+		test2.add(195);
+		System.out.println(test2.toString() +"\nis sorted: " +test2.isSorted());
+		System.out.println();
+		
+
+		
+		ListNode test3 = new ListNode();
+		test3.add(-632);
+		test3.add(-528);
+		test3.add(-348);
+		test3.add(-72);
+		test3.add(-1);
+		test3.add(0);
+		test3.add(9);
+		System.out.println(test3.toString() +"\nis sorted: " +test3.isSorted());
+		System.out.println();
+		
 	}
-	
+
 }
--- src/ListNodeTest.java
+++ src/ListNodeTest.java
@@ -1,24 +1,46 @@
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.*;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 public class ListNodeTest {
 
 	@Test
 	public void testIsSorted() {
-
 		ListNode test1 = new ListNode();
-		test1.add(6);
-		test1.add(-3);
-		test1.add(5);
-		System.out.println(test1);
-		System.out.println(test1.toString());
-		
-		isSorted dr = new isSorted();
+		test1.add(-632);
+		test1.add(-528);
+		test1.add(-348);
+		test1.add(-72);
+		test1.add(0);
+		test1.add(-8);
+		test1.add(9);
+		System.out.println(test1.toString() +"\nis sorted: " +test1.isSorted());
+		System.out.println();
 		assertFalse(test1.isSorted());
+		
+		
+		ListNode test2 = new ListNode();
+		test2.add(49);
+		test2.add(79);
+		test2.add(103);
+		test2.add(149);
+		test2.add(195);
+		System.out.println(test2.toString() +"\nis sorted: " +test2.isSorted());
+		System.out.println();
+		assertTrue(test2.isSorted());
+		
+				
+		ListNode test3 = new ListNode();
+		test3.add(-632);
+		test3.add(-528);
+		test3.add(-348);
+		test3.add(-72);
+		test3.add(-1);
+		test3.add(0);
+		test3.add(9);
+		System.out.println(test3.toString() +"\nis sorted: " +test3.isSorted());
+		System.out.println();
+		assertTrue(test3.isSorted());
 	}
-
-
 
 }
